7bbafd8a90042bdba0da214c56063dd0ec0af816
pramudia_putra_pamungkas <pramudiaputra.iv@gmail.com>
2022-11-20 16:20:32 +0700

    using text watcher to handle empty text

--- app/src/main/java/com/pramuputr/myplayground/MainFragment.kt
+++ app/src/main/java/com/pramuputr/myplayground/MainFragment.kt
@@ -1,14 +1,18 @@
 package com.pramuputr.myplayground
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.textfield.TextInputEditText
 import com.pramuputr.myplayground.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -16,4 +20,23 @@ class MainFragment : Fragment() {
         binding = FragmentMainBinding.inflate(layoutInflater)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.editText.addTextChangedListener(Watcher(binding.editText, "text is empty"))
+    }
+}
+
+class Watcher(var editText: TextInputEditText, val message: String) : TextWatcher {
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        editText.error = null
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        if (editText.text!!.isEmpty()) editText.error = message
+        else editText.error = null
+    }
+
+    override fun afterTextChanged(p0: Editable?) {}
+
 }
